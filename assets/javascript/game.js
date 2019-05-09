84d903494931b04bb8d088e868d6279ffd2b4f62
Ellen N Evans <ellennewellevans@MacPro-EEvans.local>
2019-05-09 16:18:05 -0400

    linked js and pseudocoded

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//game with 4 crystals and they each have a unique randomly assigned value from 1 - 20
+//there is a randomly assigned target score of 50 - 100
+//new numbers will generate each game
+//each value adds to the previous selected value
+//once the total is at or above the random target score, the game ends
+//if the current score is equal to the target score, the player wins
+//if the total score exceeds the target score, the player loses
+//wins and loses should be tallied on screen, even when the game resets
